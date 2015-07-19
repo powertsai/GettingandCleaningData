f1bf21431eb2f3146f3e1eac222301301832fbb9
powertsai <powertsai@gmail.com>
2015-07-19 13:02:46 +0800

    update

---
+++ loadLibrary.R
@@ -0,0 +1,28 @@
+
+#check library installed then load required library 
+loadLibrary <- function(instpkg) {
+        if(!instpkg %in% installed.packages() ){
+                install.packages(instpkg)
+        }
+        # load require library
+        (require(instpkg, character.only = TRUE))
+}
+
+#load data.table develop version 1.9.5 for fread
+loadDevDataTable <- function(instpkg) {
+        if(!instpkg %in% installed.packages()  ){
+                #install dev version of data.table
+                loadLibrary("devtools")
+                install_github("Rdatatable/data.table", build_vignettes = FALSE)
+        } else {
+                if(packageVersion(instpkg) != "1.9.5") {
+                        #remove package and install development version
+                        remove.packages(instpkg)         # First remove the current version
+                        #install dev version of data.table
+                        loadLibrary("devtools")                        
+                        install_github("Rdatatable/data.table", build_vignettes = FALSE)                        
+                }
+        }
+        #require libryay data.table
+        require(instpkg)
+}
